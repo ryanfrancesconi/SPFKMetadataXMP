901ab1c30a94c28e6650a5df57276c3e6f1cf57c
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-07 08:56:28 -0800

    refactor cleanup

--- Sources/SPFKMetadataXMPC/include/XMPFile.h
+++ Sources/SPFKMetadataXMPC/include/XMPFile.h
@@ -1,4 +1,4 @@
-// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/SPFKMetadataXMPC
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-metadata-xmp
 
 #ifndef XMPFile_H
 #define XMPFile_H
